38a37989dc6003e5f9b31ff79d5c36fb895d43d4
jari.tenhunen <jari.tenhunen@outlook.com>
2025-10-06 12:19:36 +0200

    seeding Albums if db empty

--- AlbumList/Migrations/20251006101140_modelUpdated.Designer.cs
+++ AlbumList/Migrations/20251006101140_modelUpdated.Designer.cs
@@ -3,6 +3,7 @@ using AlbumList.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AlbumList.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251006101140_modelUpdated")]
+    partial class modelUpdated
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- AlbumList/Models/Album.cs
+++ AlbumList/Models/Album.cs
@@ -6,7 +6,8 @@ namespace AlbumList.Models
     {
         [Key]
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Band { get; set; }
+        public string AlbumName { get; set; }
         public Genre MusicGenre { get; set; }
         public int TrackCount { get; set; }
         public int YearPublished { get; set; }
