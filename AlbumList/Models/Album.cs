767ed86d1793d5e0c25dd0ddc526c1ae83ba9cf6
jari.tenhunen <jari.tenhunen@outlook.com>
2025-10-06 11:22:17 +0200

    Album model added

---
+++ AlbumList/Models/Album.cs
@@ -0,0 +1,11 @@
+﻿namespace AlbumList.Models
+{
+    public class Album
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Genre MusicGenre { get; set; }
+        public int TrackCount { get; set; }
+        public int YearPublished { get; set; }
+    }
+}
